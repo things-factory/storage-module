3f69ae3da8ca7f954be3161b8caa7e827f5e3183
Murky <moosung.gil@hatiolab.com>
2020-05-15 15:10:58 +0900

    ...ing

--- storage-module.js
+++ storage-module.js
@@ -1,68 +1,36 @@
 const fetch = require('node-fetch')
 
-function moduleSelector(aSettings) {
-  var toReturn
-  if (aSettings.storageModule) {
-    if (typeof aSettings.storageModule === 'string') {
-      // TODO: allow storage modules to be specified by absolute path
-      toReturn = require('./' + aSettings.storageModule)
-    } else {
-      toReturn = aSettings.storageModule
-    }
-  } else {
-    toReturn = require('./localfilesystem')
-  }
-  return toReturn
-}
-
-function is_malicious(path) {
-  return path.indexOf('../') != -1 || path.indexOf('..\\') != -1
-}
-
 const thingsFactoryStorage = {
   init: async function (settings, runtime) {
     thingsFactoryStorage.settings = settings
     thingsFactoryStorage.runtime = runtime
 
-    // if (!settings.thingsFactoryStorageBucket || !settings.googleProjectId || !settings.googleCredentials) {
-    //   reject('Cannot initialize without Google Cloud settings')
-    // } else {
-    //   //console.log('---- *** node-red google cloud storage got all needed settings! *** ---')
-    //   if (settings.flowFile) {
-    //     flowFile = settings.flowFile.replace('.json', '')
-    //   }
-    //   thingsFactoryStorage.getBucket().then(() => {
-    //     if (settings.googleFirebaseReload === true) {
-    //       thingsFactoryStorage.setupFirebaseListener()
-    //     }
-    //     thingsFactoryStorage.prepopulateFlows(resolve)
-    //   })
-    // }
+    const { serverUrl, user } = this.settings.thingsFactory
+    const { username: email, password } = user
 
-    const { username: email, password } = settings.thingsFactory.user
-
-    const response = await fetch(`http://localhost:3000/signin`, {
+    const response = await fetch(`${serverUrl}/signin`, {
       method: 'POST',
       credentials: 'include',
       body: JSON.stringify({
         email,
         password
       }),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json', 'x-only-token': true }
     })
 
+    this.accessToken = await response.text()
+
     return
-
-    // return new Promise((resolve, reject) => {
-
-    // })
   },
 
   getFlows: async function () {
-    const myDomain = this.settings.thingsFactory.user.domain
+    const { serverUrl, user } = this.settings.thingsFactory
+    const { domain: myDomain } = user
 
-    const response = await fetch(`http://localhost:3000/get-flows/${myDomain}`, {
-      credentials: 'include'
+    const response = await fetch(`${serverUrl}/get-flows/${myDomain}`, {
+      headers: {
+        authorization: this.accessToken
+      }
     })
     const json = await response.json()
     const flow = json.flow
@@ -71,21 +39,21 @@ const thingsFactoryStorage = {
   },
 
   saveFlows: async function (flows) {
-    const myDomain = this.settings.thingsFactory.user.domain
+    const { serverUrl, user } = this.settings.thingsFactory
+    const { domain: myDomain } = user
 
-    const response = await fetch(`http://localhost:3000/save-flows/${myDomain}`, {
+    const response = await fetch(`${serverUrl}/save-flows/${myDomain}`, {
       method: 'POST',
       body: JSON.stringify({
         flows
       }),
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' }
+      headers: { 'Content-Type': 'application/json', authorization: this.accessToken }
     })
     return true
   },
 
   getCredentials: async function () {
-    console.log('------------------------------------------- getCredentials', this.settings, this.settings.adminAuth)
+    console.log('------------------------------------------- getCredentials')
     return true
   }
 }
